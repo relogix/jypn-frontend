3ac8e78be3ab6e4583a24afae9182c457fb748b4
Luthfi Hizb <luthfihizb@gmail.com>
2021-12-12 10:32:16 +0700

    Test: Member Page

--- src/pages/public/Members/components/MemberPage/index.js
+++ src/pages/public/Members/components/MemberPage/index.js
@@ -4,35 +4,35 @@ import { useNavigate, useParams } from "react-router";
 import { useState } from "react/cjs/react.development";
 import { publicRouteSlug } from "../../../../../routes/publicRoutes";
 import DesktopVersion from "./DesktopVersion";
+import MobileVersion from "./MobileVersion";
 import Biography from "./memberSections/Biography";
 import Profile from "./memberSections/Profile";
 import Title from "./memberSections/Title";
-import MobileVersion from "./MobileVersion";
 
 const MemberPage = () => {
   const navigate = useNavigate();
   const { memberName } = useParams();
 
   const [member, setMember] = useState();
-  useEffect(() => {
-    let mounted = true;
-    console.log("effect run");
-    memberName &&
-      axios
-        .get(`${process.env.REACT_APP_API}/api/member-details?filters[member][name][$eq]=${memberName}&populate=*`)
-        .then((res) => {
-          console.log("response run");
-          if (mounted) {
-            console.log("mounted run");
-            console.log(res.data);
-            res.data?.data?.length ? setMember(res.data?.data[0]) : navigate(publicRouteSlug.MEMBERS);
-          }
-        });
+  // useEffect(() => {
+  //   let mounted = true;
+  //   console.log("effect run");
+  //   memberName &&
+  //     axios
+  //       .get(`${process.env.REACT_APP_API}/api/member-details?filters[member][name][$eq]=${memberName}&populate=*`)
+  //       .then((res) => {
+  //         console.log("response run");
+  //         if (mounted) {
+  //           console.log("mounted run");
+  //           console.log(res.data);
+  //           res.data?.data?.length ? setMember(res.data?.data[0]) : navigate(publicRouteSlug.MEMBERS);
+  //         }
+  //       });
 
-    return () => {
-      mounted = false;
-    };
-  }, [memberName]);
+  //   return () => {
+  //     mounted = false;
+  //   };
+  // }, [memberName]);
 
   // Sections
   const sections = [
@@ -52,12 +52,13 @@ const MemberPage = () => {
 
   return (
     <div>
-      <div className="hidden md:inline">
+      asdasdasdasdasd
+      {/* <div className="hidden md:inline">
         <DesktopVersion member={member} memberName={memberName} sections={sections} />
       </div>
       <div className="md:hidden">
         <MobileVersion member={member} memberName={memberName} sections={sections} />
-      </div>
+      </div> */}
     </div>
   );
 };
